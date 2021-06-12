0a9eecb27176c15945b722bb3552da3fb39870ed
markahenrich <mark.henrich@icloud.com>
2021-06-12 00:12:27 -0700

    Add navigation view, add image view, disable rotation

---
+++ Andromeda/Andromeda/APIKey.swift
@@ -0,0 +1,13 @@
+//
+//  APIKey.swift
+//  Andromeda
+//
+//  Created by Mark Henrich on 6/11/21.
+//
+
+import Foundation
+
+// I know this is bad practice but this application is for demonstration purposes
+// and will never be released publicly.
+
+let APIKey: String = "1Rba9DanrD8fZLyHyxHmUsDXPrzNTgEadv0qUuv7"
--- Andromeda/Andromeda/ViewController.swift
+++ Andromeda/Andromeda/ViewController.swift
@@ -8,12 +8,30 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var picOfTheDay: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
-        // Test Comment
+        title = "Picture of the Day: "
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        loadPicOfDay()
+
+    }
+    
+    func loadPicOfDay() {
+        DispatchQueue.global().async { [weak self] in
+            
+            if let data = try? Data(contentsOf: URL(string: "https://apod.nasa.gov/apod/image/2106/JunSE_DSC_7477b_1024.jpg")!) {
+                if let image = UIImage(data: data) {
+                    DispatchQueue.main.async {
+                        self?.picOfTheDay.image = image
+                    }
+                }
+            }
+            
+        }
     }
 
 
