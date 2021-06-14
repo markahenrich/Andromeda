42a69129cb3a2e34f8888f43453a31782aa15c37
markahenrich <mark.henrich@icloud.com>
2021-06-14 04:26:47 -0700

    add detail view for APOD, add test image, add APODDetailViewController

--- Andromeda/Andromeda/Controllers/HomeViewController.swift
+++ Andromeda/Andromeda/Controllers/HomeViewController.swift
@@ -9,10 +9,18 @@ import UIKit
 
 class HomeViewController: UIViewController {
     @IBOutlet var picOfTheDay: UIImageView!
+    @IBOutlet var learnMoreBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        picOfTheDay.image = UIImage(named: "test")
+        learnMoreBtn.layer.cornerRadius = 15
+        learnMoreBtn.clipsToBounds = true
     }
+    
+    @IBAction func didTapImageView(_ sender: UITapGestureRecognizer) {
+        print("tapped")
+    }
+    
 }
-
