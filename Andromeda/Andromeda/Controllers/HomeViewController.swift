7f19b2978bce90950b285cd38a86da1c7fc67f48
markahenrich <mark.henrich@icloud.com>
2021-06-17 23:25:31 -0700

    remove image zoom (practically useless feature in current state)

--- Andromeda/Andromeda/Controllers/HomeViewController.swift
+++ Andromeda/Andromeda/Controllers/HomeViewController.swift
@@ -18,23 +18,4 @@ class HomeViewController: UIViewController {
         picOfTheDay.image = image
         
     }
-    
-    @IBAction func didTapImageView(_ sender: UITapGestureRecognizer) {
-        let imageView = sender.view as! UIImageView
-        let newImageView = UIImageView(image: imageView.image)
-        newImageView.frame = UIScreen.main.bounds
-        newImageView.backgroundColor = UIColor.black
-        newImageView.isUserInteractionEnabled = true
-        let tap = UITapGestureRecognizer(target: self, action: #selector(dismissFullscreenImage))
-        newImageView.addGestureRecognizer(tap)
-        newImageView.contentMode = .scaleAspectFill
-        self.view.addSubview(newImageView)
-        self.navigationController?.isNavigationBarHidden = true
-        self.tabBarController?.tabBar.isHidden = true
-    }
-    @objc func dismissFullscreenImage(sender: UITapGestureRecognizer) {
-        self.navigationController?.isNavigationBarHidden = false
-        self.tabBarController?.tabBar.isHidden = false
-        sender.view?.removeFromSuperview()
-    }
 }
